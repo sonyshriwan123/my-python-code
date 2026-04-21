03f33910725be14fa15d5c1dd85e55553d698e7f
sonyshriwan123 <sonikashriwan216@gmail.com>
2026-04-21 16:17:16 +0530

    updated code

--- numpy/second.py
+++ numpy/second.py
@@ -30,6 +30,60 @@ import numpy as np
 # print(newarr.ndim)
 
 
-arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
-newarr = arr.reshape(-2)
-print(newarr)
+# arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+# newarr = arr.reshape(-2)
+# print(newarr)
+
+# ==iterating though a numpy array===
+# arr = np.array([[[1,2,3], [4,5,6],[7,8,9,],[10,11,12,]]])
+# for x in arr:
+#     for y in x:
+#         for z in y:
+#             print(z)
+            
+# arr = np.array([[[1,2,3], [4,5,6],[7,8,9,],[10,11,12]]])
+# for x in np.nditer(arr):
+#     print(x)
+
+
+# join the two arrays
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([4,5,6])
+# arr3 = np.concatenate((arr1,arr2))
+# print(arr3)
+
+# arr4 = np.array([[1,2,3],[4,5,6]])
+# arr5 = np.array([[7,8,9],[10,11,12]])
+# arr6 = np.concatenate((arr4,arr5), axis=1)
+# print(arr6)
+# arr4 = np.array([[1,2,3],[4,5,6]])
+# arr5 = np.array([[7,8,9],[10,11,12]])
+# arr6 = np.concatenate((arr4,arr5), axis=0)
+# print(arr6)
+
+# splitting the array
+# arr = np.array([1,2,3,4,5,6])
+# newarr = np.array_split(arr,2)
+# print(newarr)
+# arr = np.array([1,2,3,4,5,6])
+# newarr = np.array_split(arr,3)
+# print(newarr)
+
+# arr = np.array([1, 2, 3, 4, 5, 6])
+
+# newarr = np.array_split(arr, 3)
+
+# print(newarr[0])
+# print(newarr[1])
+# print(newarr[2]) 
+
+# ===searching arrys======
+# arr = np.array([1,2,3,4,5,6,7,8,9,10])
+# x = np.where(arr == 4)
+# print(x)
+
+# arr = np.array([10, 14, 93, 41, 8, 7])
+
+# x = np.where(arr%2 == 0)
+
+# print(x)
