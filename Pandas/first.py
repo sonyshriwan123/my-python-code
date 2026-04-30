a08cf5ed4d11ec42ff86e1b201968a0abbe2a9b4
sonyshriwan123 <sonikashriwan216@gmail.com>
2026-04-30 16:08:44 +0530

    update code

---
+++ Pandas/first.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# mydataset = {
+#     'cars' : ['BMW','Volve','Ford'],
+#     'passings' : [3,7,2]
+# }
+
+# myvar = pd.DataFrame(mydataset)
+# print(myvar)
+# print("The version is:", pd.__version__)
+
+# x = [1,2,3,4,5,6]
+# var = pd.Series(x)
+
+# print(var)
+# print(var[0])
+# print(type(var))
+
+# x = [1,2,3,4]
+# var = pd.Series(x, index = ['x','y','z','w'])
+# print(var)
+# print("finding the value:", var['z'])
